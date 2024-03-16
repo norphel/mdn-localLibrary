0ad7bfbcbb1a2ba86d7863ba32f350ea3283a9eb
Norphel <113838216+norphel@users.noreply.github.com>
2024-03-16 11:07:45 +0530

    implement delete author controller and view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -83,11 +83,41 @@ exports.author_create_post = [
 
 // Display Author delete form on GET
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    res.redirect("/catalog/authors");
+  }
+
+  res.render("author_delete", {
+    title: "Delete Author",
+    author: author,
+    author_books: allBooksByAuthor,
+  });
 });
 
 // Handle Author delete on POST
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (allBooksByAuthor.length > 0) {
+    res.render("author_delete", {
+      title: "Delete Author",
+      author: author,
+      author_books: allBooksByAuthor,
+    });
+    return;
+  } else {
+    await Author.findByIdAndDelete(req.body.authorid);
+    res.redirect("/catalog/authors");
+  }
+
   res.send("NOT IMPLEMENTED: Author delete POST");
 });
 
